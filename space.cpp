8acbe84a4e838291a4e332c536ca05e34b2ba0ab
cheralynli <cheralynlianto@gmail.com>
2026-01-31 20:55:02 +0800

    first trial

---
+++ space.cpp
@@ -0,0 +1,21 @@
+#include "space.h"
+#include <string>
+#include <map>
+
+namespace space_age {
+
+double ages(double age,std::string planet) {
+    std::map<std::string,int> planets = {
+        {"Mercury",0.2408467},
+        {"Venus",0.61519726},
+        {"Earth",1.0},
+        {"Mars",1.8808158},
+        {"Jupiter",11.862615},
+        {"Saturn",29.447498},
+        {"Uranus",84.016846},
+        {"Neptune",164.79132}
+    }
+    return planets[planet]*age/31557600;
+}
+
+}  // namespace space_age
---
+++ space.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <map>
+
+namespace space_age {
+
+double ages(double age,std::string planet);
+
+
+}  // namespace space_age
